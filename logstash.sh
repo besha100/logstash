ceb9f43f47e9a09d2abbc330d7f87b8dffa76ce3
besha100 <36448614+besha100@users.noreply.github.com>
2020-08-07 16:48:01 +0200

    Update logstash.sh

--- logstash.sh
+++ logstash.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo apt-get install default-jre
 sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt-get install apt-transport-https
 sudo echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
