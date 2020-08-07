2b1db0afca9bc8b24418a7dafe47bf122338d6bd
bisha <mohamed.basher@rebtel.com>
2020-08-07 16:47:06 +0200

    added Java

--- logstash.sh
+++ logstash.sh
@@ -3,7 +3,7 @@ sudo apt-get install default-jre
 sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt-get install apt-transport-https
 sudo echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-sudo apt-get install default-jre
+sudo apt install default-jre
 sudo apt-get update && sudo apt-get install logstash
 sudo rm -rf /etc/logstash/conf.d/logstash.conf 
 sudo cp logstash.conf /etc/logstash/conf.d/ && sudo cp suricata /etc/logrotate.d/ && sudo logrotate /etc/logrotate.d/suricata && sudo chmod 775 /var/log/suricata/eve.json 
