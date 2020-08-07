289fb9286e8cc36745b0147a40ffdd5097b5ec7e
besha100 <36448614+besha100@users.noreply.github.com>
2020-08-07 17:07:02 +0200

    Update logstash.sh

--- logstash.sh
+++ logstash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https -y
 sudo echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt install default-jre -y
-sudo apt-get update && sudo apt-get install logstash
+sudo apt-get update -y && sudo apt-get install logstash -y
 sudo rm -rf /etc/logstash/conf.d/logstash.conf 
 sudo cp logstash.conf /etc/logstash/conf.d/ && sudo cp suricata /etc/logrotate.d/ && sudo logrotate /etc/logrotate.d/suricata && sudo chmod 775 /var/log/suricata/eve.json 
