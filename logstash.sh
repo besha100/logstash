bfe93ffd23b94d1b9682964e177914bb3914b973
bisha <mohamed.basher@rebtel.com>
2020-08-07 15:51:11 +0200

    added Java

--- logstash.sh
+++ logstash.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get install default-jre
 sudo curl -L -O https://artifacts.elastic.co/downloads/logstash/logstash-5.2.0.deb && sudo dpkg -i logstash-5.2.0.deb
 sudo rm -rf /etc/logstash/conf.d/logstash.conf 
 sudo cp logstash.conf /etc/logstash/conf.d/ && sudo cp suricata /etc/logrotate.d/ && sudo logrotate /etc/logrotate.d/suricata && sudo chmod 775 /var/log/suricata/eve.json 
